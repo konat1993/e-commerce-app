6a206bd88072d1f5c9308ac13e767a918ba4757e
Lukasz Konatowski <konat1993@gmail.com>
2022-04-25 22:51:56 +0200

    NavBar and Footer applied

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>
+          SoundCore Headphones
+        </Link>
+      </p>
+      <button className="cart-icon" onClick={''}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
   )
 }
 
