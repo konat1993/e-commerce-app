c7cafb63da36b4a9459f36034f3ae0efbda441b2
Lukasz Konatowski <konat1993@gmail.com>
2022-04-25 19:12:14 +0200

    hero banner component basic structure

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,8 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p className="beats-solo">
+          SMALLTEXT
+        </p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className='hero-banner-image' />
+        <div>
+          <Link href='/product/ID' passHref>
+            <button type='button'>BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
